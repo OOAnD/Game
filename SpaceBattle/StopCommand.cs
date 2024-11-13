b899683fe1d9b98d3503f696cb88205d7c9999ed
Alexey Rau <arau729@gmail.com>
2024-11-14 05:04:48 +0600

    feat: Добавлена реализация StopCommand

---
+++ SpaceBattle/StopCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle
+{
+    public class StopCommand : ICommand
+    {
+
+        private readonly ICommandBox _cancellableCommand;
+        private readonly IEmptyCommandFactory _emptyCommandFactory;
+
+        public StopCommand(ICommandBox cancellableCommand, IEmptyCommandFactory emptyCommandFactory)
+        {
+            _cancellableCommand = cancellableCommand ?? throw new ArgumentNullException(nameof(cancellableCommand));
+            _emptyCommandFactory = emptyCommandFactory ?? throw new ArgumentNullException(nameof(emptyCommandFactory));
+        }
+
+        public void Execute()
+        {
+            var emptyCommand = _emptyCommandFactory.CreateEmptyCommand() ?? throw new InvalidOperationException();
+            _cancellableCommand.Set(emptyCommand);
+        }
+    }
+}
