d72f5ce88709485fd3237e3aa17dbf944b21811f
ezyxip <anzej2003@gmail.com>
2024-11-13 15:20:50 +0600

    refactor: Исправлено форматирование

--- SpaceBattle/ICommand.cs
+++ SpaceBattle/ICommand.cs
@@ -1,5 +1,7 @@
-namespace SpaceBattle{
-    public interface ICommand{
+﻿namespace SpaceBattle
+{
+    public interface ICommand
+    {
         void Execute();
     }
 }
--- SpaceBattle/IEmptyCommandFactory.cs
+++ SpaceBattle/IEmptyCommandFactory.cs
@@ -1,4 +1,4 @@
-namespace  SpaceBattle
+﻿namespace SpaceBattle
 {
     public interface IEmptyCommandFactory
     {
