a2c1e64631706121182866a2df924e0030d6af74
ezyxip <anzej2003@gmail.com>
2024-11-13 15:21:12 +0600

    feat: Реализовано базовое поведение StopCommand

---
+++ SpaceBattle.Tests/StopCommandTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+
+namespace SpaceBattle.Test
+{
+    public class StopCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldCancelCommand()
+        {
+            // Arrange
+            var cancelable = new Mock<ICommandBox>();
+            var emptyCommand = new Mock<ICommand>();
+
+            var utilCommandFactory = new Mock<IEmptyCommandFactory>();
+
+            utilCommandFactory.Setup(f => f.CreateEmptyCommand()).Returns(emptyCommand.Object);
+
+            var stopCommand = new StopCommand(cancelable.Object, utilCommandFactory.Object);
+
+            // Act
+            stopCommand.Execute();
+
+            // Assert
+            cancelable.Verify(c => c.Set(emptyCommand.Object), Times.Once);
+        }
+    }
+}
---
+++ SpaceBattle/StopCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle
+{
+    public class StopCommand : ICommand
+    {
+
+        private readonly ICommandBox _cancellableCommand;
+        private readonly IEmptyCommandFactory _emptyCommandFactory;
+
+        public StopCommand(ICommandBox cancellableCommand, IEmptyCommandFactory emptyCommandFactory)
+        {
+            _cancellableCommand = cancellableCommand;
+            _emptyCommandFactory = emptyCommandFactory;
+        }
+
+        public void Execute()
+        {
+            var emptyCommand = _emptyCommandFactory.CreateEmptyCommand();
+            _cancellableCommand.Set(emptyCommand);
+        }
+    }
+}
