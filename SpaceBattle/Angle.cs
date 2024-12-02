385f81bb8ba8da983953a48f8859cd71d17a4664
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-02 20:05:29 +0600

    fix: Починил класс Angle

--- SpaceBattle/Angle.cs
+++ SpaceBattle/Angle.cs
@@ -1,18 +1,46 @@
-namespace SpaceBattle{
-    public class Angle{
+﻿namespace SpaceBattle
+{
+    public class Angle
+    {
         public int _value;
-        public Angle(int value){
-            _value = value;
+        public int _border;
+        public int Value
+        {
+            get => _value;
+            set => _value = value;
         }
-        public int GetValue(){
-            return _value;
-        }
+        public int Border => _border;
+        public Angle(int value, int border = 360)
+        {
+            if (border < 0)
+            {
+                throw new Exception("Wrong, can not be zero or less");
+            }
+            else
+            {
+                _border = border;
+            }
 
-        public Angle Sum(Angle additiveAngle){
-            var valueToAdd = additiveAngle.GetValue();
-            var sumOfValues = _value + valueToAdd;
-            var newAngle = new Angle(sumOfValues);
-            return newAngle;
+            _value = ((value % _border) + _border) % _border;
+        }
+        public static Angle operator +(Angle angle, Angle velocity)
+        {
+            var denominator = angle.Border * velocity.Border; // тут надо по другому
+            var numerator = angle.Value * velocity.Border + angle.Border * velocity.Value;
+
+            var gcd = GCD(denominator, numerator);
+            numerator /= gcd;
+            denominator /= gcd;
+
+            return new Angle(numerator, denominator);
+        }
+        private static int GCD(int a, int b)
+        {
+        return Enumerable.Range(0, int.MaxValue)
+                         .Select(i => new { A = a, B = b })
+                         .TakeWhile(pair => pair.A != 0 && pair.B != 0)
+                         .Aggregate((a, b) => new { A = a.A > a.B ? a.A % a.B : a.A, B = a.B > a.A ? a.B % a.A : a.B })
+                         .A | (a | b);
         }
     }
 }
