6503a21b2c89c50c2cf3162a6649690d65d4abf1
Romanova Angelina <7romanovaangelina7@gmail.com>
2024-12-02 15:49:48 +0300

    fix: Убраны лишние тесты

--- SpaceBattle.Tests/MoveCommandTest.cs
+++ SpaceBattle.Tests/MoveCommandTest.cs
@@ -4,7 +4,7 @@ namespace SpaceBattle.Tests
     public class MoveCommandTest
     {
         [Fact]
-        public void Execute_ShouldMoveObject_WithValidInput()
+        public void Execute_ShouldMoveObject_WithValidValue()
         {
             // Arrange
             var movingObject = new Mock<IMoving>();
@@ -27,97 +27,6 @@ namespace SpaceBattle.Tests
 
             // Assert
             Assert.Equal(correctPosition, startPosition);
-        }
-
-        [Fact]
-        public void Execute_ShouldNotMoveObject_WithZeroVelocity()
-        {
-            // Arrange
-            var movingObject = new Mock<IMoving>();
-            var startPosition = new Vector(2);
-            var velocity = new Vector(2);
-
-            startPosition.Set(1, 2);
-            velocity.Set(0, 0);
-
-            movingObject.SetupGet(m => m.Position).Returns(startPosition);
-            movingObject.SetupGet(m => m.Velocity).Returns(velocity);
-            movingObject.SetupSet(m => m.Position = It.IsAny<Vector>()).Callback<Vector>(pos => startPosition = pos);
-
-            var moveCommand = new MoveCommand(movingObject.Object);
-
-            // Act
-            moveCommand.Execute();
-
-            // Assert
-            Assert.Equal(startPosition, startPosition);
-        }
-
-        [Fact]
-        public void Execute_ShouldMoveObjectBackwards_WithNegativeVelocity()
-        {
-            // Arrange
-            var movingObject = new Mock<IMoving>();
-            var startPosition = new Vector(2);
-            var velocity = new Vector(2);
-            var correctPosition = new Vector(2);
-
-            startPosition.Set(1, 2);
-            velocity.Set(-3, -4);
-            correctPosition.Set(-2, -2);
-
-            movingObject.SetupGet(m => m.Position).Returns(startPosition);
-            movingObject.SetupGet(m => m.Velocity).Returns(velocity);
-            movingObject.SetupSet(m => m.Position = It.IsAny<Vector>()).Callback<Vector>(pos => startPosition = pos);
-
-            var moveCommand = new MoveCommand(movingObject.Object);
-
-            // Act
-            moveCommand.Execute();
-
-            // Assert
-            Assert.Equal(correctPosition, startPosition);
-        }
-
-        [Fact]
-        public void Execute_ShouldThrowException_WithDifferentDimensions()
-        {
-            // Arrange
-            var movingObject = new Mock<IMoving>();
-            var startPosition = new Vector(2);
-            var velocity = new Vector(3);
-
-            startPosition.Set(1, 2);
-            velocity.Set(3, 4, 5);
-
-            movingObject.SetupGet(m => m.Position).Returns(startPosition);
-            movingObject.SetupGet(m => m.Velocity).Returns(velocity);
-
-            var moveCommand = new MoveCommand(movingObject.Object);
-
-            // Act & Assert
-            Assert.Throws<ArgumentException>(() => moveCommand.Execute());
-        }
-
-        [Fact]
-        public void Execute_ShouldNotMoveObject_WithZeroDimension()
-        {
-            // Arrange
-            var movingObject = new Mock<IMoving>();
-            var startPosition = new Vector(0);
-            var velocity = new Vector(0);
-
-            movingObject.SetupGet(m => m.Position).Returns(startPosition);
-            movingObject.SetupGet(m => m.Velocity).Returns(velocity);
-            movingObject.SetupSet(m => m.Position = It.IsAny<Vector>()).Callback<Vector>(pos => startPosition = pos);
-
-            var moveCommand = new MoveCommand(movingObject.Object);
-
-            // Act
-            moveCommand.Execute();
-
-            // Assert
-            Assert.Equal(startPosition, startPosition);
         }
 
         [Fact]
