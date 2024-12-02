2daf7c0a1a2ff7b9ed42d90927ece9ce3be7a250
Alexey Rau <arau729@gmail.com>
2024-12-02 20:24:17 +0600

    feat: Добавлен StopCommand

---
+++ SpaceBattle/StopCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceBattle
+{
+    public class StopCommand : ICommand
+    {
+        private readonly ICommandBox _cancellableCommand;
+
+        public StopCommand(ICommandBox cancellableCommand)
+        {
+            _cancellableCommand = cancellableCommand ?? throw new ArgumentNullException(nameof(cancellableCommand));
+        }
+
+        public void Execute()
+        {
+            var emptyCommand = new EmptyCommand();
+            _cancellableCommand.Set(emptyCommand);
+        }
+    }
+}
