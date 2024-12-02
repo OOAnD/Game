285d75512a49dc06ee2e1f79961d864c744e686c
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-02 20:01:58 +0600

    feat: Добавил тестов

--- SpaceBattle.Tests/RotateCommandTest.cs
+++ SpaceBattle.Tests/RotateCommandTest.cs
@@ -1,11 +1,14 @@
-using Moq;
+﻿using Moq;
 
-namespace SpaceBattle.Tests{
+namespace SpaceBattle.Tests
+{
 
-    public class RotateCommandTest{
+    public class RotateCommandTest
+    {
         [Fact]
-        public void Execute_ShouldRotateObject(){
-            
+        public void Execute_ShouldRotateObject()
+        {
+
             //Arrange
             var rotatingObject = new Mock<IRotating>();
             var angle = new Angle(45);
@@ -13,16 +16,69 @@ namespace SpaceBattle.Tests{
             var resultAngle = new Angle(135);
             var testRotate = new RotateCommand(rotatingObject.Object);
 
-            rotatingObject.Setup(obj => obj.GetAngle()).Returns(angle);
-            rotatingObject.Setup(obj => obj.GetVelocity()).Returns(angleVelocity);
+            rotatingObject.Setup(obj => obj.Angle).Returns(angle);
+            rotatingObject.Setup(obj => obj.Velocity).Returns(angleVelocity);
 
             //Act
             testRotate.Execute();
 
             //Assert
-            rotatingObject.VerifySet(ro => ro.SetAngle(resultAngle));
-            
+            rotatingObject.VerifySet(ro => ro.Angle = resultAngle);
+
+        }
+        [Fact]
+        public void Define_ValueShouldBeGreaterThanZero()
+        {
+            //Arrange & Act
+            var angle = new Angle(-100000, 360);
+            var isPositive = angle.Value >= 0;
+
+            //Assert
+            Assert.True(isPositive);
+        }
+        [Fact]
+        public void Define_ValueShouldBeLessThanBorder()
+        {
+            //Arrange & Act
+            var angle = new Angle(100000, 360);
+            var isLessThanBorder = angle.Value < angle.Border;
+
+            //Assert
+            Assert.True(isLessThanBorder);
+        }
+        [Fact]
+        public void Define_BorderShouldBeGreaterThanZero()
+        {
+            //Arrange & Act & Assert
+            Assert.Throws<Exception>(() => new Angle(90, -360));
+        }
+        [Fact]
+        public void Sum_ShouldBeGreaterThanZero()
+        {
+            //Arrange
+            var angle1 = new Angle(100, 120);
+            var angle2 = new Angle(120, 150);
+
+            //Act
+            var angleSum = angle1 + angle2;
+            var isGreaterThanZero = angleSum.Value >= 0;
+
+            //Assert
+            Assert.True(isGreaterThanZero);
+        }
+        [Fact]
+        public void Sum_ShouldBeLessThanBorder()
+        {
+            //Arrange
+            var angle1 = new Angle(100, 120);
+            var angle2 = new Angle(120, 150);
+            //Act
+            var angleSum = angle1 + angle2;
+            var isLessThanBorder = angleSum.Value < angleSum.Border;
+
+            //Assert
+            Assert.True(isLessThanBorder);
+
         }
     }
-
 }
