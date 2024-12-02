8c70e481cd7cab3c11b5c358a68052cfa92b9dbc
Alexey Rau <arau729@gmail.com>
2024-12-02 20:24:34 +0600

    feat: Добавлены тесты

---
+++ SpaceBattle.Tests/StopCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+namespace SpaceBattle.Tests
+{
+    public class StopCommandTests
+    {
+        [Fact]
+        public void Execute_ShouldSetEmptyCommand_WhenCalled()
+        {
+            // Arrange
+            var mockCommandBox = new Mock<ICommandBox>();
+            var stopCommand = new StopCommand(mockCommandBox.Object);
+
+            // Act
+            stopCommand.Execute();
+
+            // Assert
+            mockCommandBox.Verify(cb => cb.Set(It.IsAny<EmptyCommand>()), Times.Once);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentNullException_WhenCommandBoxIsNull()
+        {
+            // Arrange & Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => new StopCommand(null));
+            Assert.Equal("cancellableCommand", exception.ParamName);
+        }
+
+        [Fact]
+        public void Execute_ShouldNotThrow_WhenCommandBoxIsValid()
+        {
+            // Arrange
+            var mockCommandBox = new Mock<ICommandBox>();
+            var stopCommand = new StopCommand(mockCommandBox.Object);
+
+            // Act
+            var exception = Record.Exception(() => stopCommand.Execute());
+
+            // Assert
+            Assert.Null(exception);
+        }
+    }
+}
