3d9eb8edbd0af6c4585d028f76bed662b110c209
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-02 20:05:42 +0600

    fix: Починил команду

--- SpaceBattle/RotateCommand.cs
+++ SpaceBattle/RotateCommand.cs
@@ -1,15 +1,18 @@
-namespace SpaceBattle{
-    public class RotateCommand : ICommand{
+﻿namespace SpaceBattle
+{
+    public class RotateCommand : ICommand
+    {
         public IRotating _rotatingObject;
-        public RotateCommand(IRotating rotatingObject){
+        public RotateCommand(IRotating rotatingObject)
+        {
             _rotatingObject = rotatingObject;
         }
-        public void Execute(){
-            var currentAngle = _rotatingObject.GetAngle();
-            var velocity = _rotatingObject.GetVelocity();
-            var newAngle = currentAngle.Sum(velocity);
-            _rotatingObject.SetAngle(newAngle);
+        public void Execute()
+        {
+            var currentAngle = _rotatingObject.Angle;
+            var velocity = _rotatingObject.Velocity;
+            var newAngle = currentAngle + velocity;
+            _rotatingObject.Angle = newAngle;
         }
     }
-
 }
