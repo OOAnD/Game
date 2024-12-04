17efa65f5d9bed36a94f6178aaa29565ffc13043
ezyxip <anzej2003@gmail.com>
2024-12-04 08:25:50 +0600

    feat: Реализован MacroCommand

---
+++ SpaceBattle/MacroCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattle
+{
+
+    public class MacroCommand : ICommand
+    {
+        private readonly IEnumerable<ICommand> _commands;
+
+        public MacroCommand(params ICommand[] commands)
+        {
+            if (commands.Any(c => c == null))
+            {
+                throw new ArgumentNullException(nameof(commands), "Команды не могут быть null.");
+            }
+
+            _commands = [.. commands];
+        }
+
+        public void Execute() => _commands.ToList().ForEach(c => c.Execute());
+    }
+}
