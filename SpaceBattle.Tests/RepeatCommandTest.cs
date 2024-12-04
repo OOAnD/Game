fad230d162a7c8097faee2e7a5289e06d321e432
ezyxip <anzej2003@gmail.com>
2024-12-04 08:26:52 +0600

    feat: Реализован RepeatCommand

---
+++ SpaceBattle.Tests/RepeatCommandTest.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RepeatCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldRepeatCommand()
+        {
+            // Arrange
+            var command = new Mock<ICommand>();
+            var queue = new Mock<ICommandQueue>();
+            var repeatCommand = new RepeatCommand(queue.Object, command.Object);
+
+            // Act
+            repeatCommand.Execute();
+
+            // Assert
+            queue.Verify(q => q.Enqueue(command.Object), Times.Once);
+        }
+
+        [Fact]
+        public void Define_ShouldThrow_WhenCommandIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new RepeatCommand(new Mock<ICommandQueue>().Object, null!));
+        }
+
+        [Fact]
+        public void Define_ShouldThrow_WhenQueueIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new RepeatCommand(null!, new Mock<ICommand>().Object));
+        }
+
+        [Fact]
+        public void Execute_ShouldThrow_WhenEnqueueThrow()
+        {
+            // Arrange
+            var command = new Mock<ICommand>();
+            var queue = new Mock<ICommandQueue>();
+            queue.Setup(q => q.Enqueue(command.Object)).Throws(new Exception());
+            var repeatCommand = new RepeatCommand(queue.Object, command.Object);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => repeatCommand.Execute());
+        }
+    }
+}
---
+++ SpaceBattle/RepeatCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle
+{
+    public class RepeatCommand(ICommandQueue queue, ICommand command) : ICommand
+    {
+        private readonly ICommandQueue _queue = queue
+            ?? throw new ArgumentNullException(nameof(queue));
+        private readonly ICommand _command = command
+            ?? throw new ArgumentNullException(nameof(command));
+
+        public void Execute() => _queue.Enqueue(_command);
+    }
+}
