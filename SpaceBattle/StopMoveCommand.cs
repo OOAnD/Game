bd92ce5a07cc179477af6310a6c33d033c4a329c
Rau Alexey <rauleha2729@gmail.com>
2024-12-04 09:33:25 +0600

    feat: Добавлен StopMoveCommand

---
+++ SpaceBattle/StopMoveCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceBattle
+{
+    public class StopMoveCommand : ICommand
+    {
+        private readonly IDictionary<string, object> _gameObject;
+
+        public StopMoveCommand(IDictionary<string, object> gameObject)
+        {
+            _gameObject = gameObject ?? throw new ArgumentNullException(nameof(gameObject));
+        }
+
+        public void Execute()
+        {
+            var cancelableCommand = (ICommandBox)_gameObject["Movement"];
+            cancelableCommand.Set(new EmptyCommand());
+        }
+    }
+}
