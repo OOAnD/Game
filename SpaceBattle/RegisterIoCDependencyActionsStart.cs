840ada10b5caa1ee70639b382e8b18897f2099d9
ezyxip <anzej2003@gmail.com>
2024-12-10 12:21:27 +0600

    efat: Реализована команда, регистрирующая зависимость Actions.Start

---
+++ SpaceBattle/RegisterIoCDependencyActionsStart.cs
@@ -0,0 +1,23 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyActionsStart : ICommand
+    {
+        public void Execute()
+        {
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Actions.Start",
+                (object[] args) => new StartCommand((string)args[0], (string)args[1], (object[])args[2])
+            ).Execute();
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Game.Object.Actions.Set",
+                (object[] args) => new SetCommand((string)args[0], (string)args[1], (ICommand)args[2])
+            ).Execute();
+        }
+    }
+}
