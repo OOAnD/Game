1bb52eb7588feb2833e0ddc43001d9008de9b5b9
ezyxip <anzej2003@gmail.com>
2024-12-10 12:15:30 +0600

    feat: Реализован CommandInjectableCommand

---
+++ SpaceBattle.Tests/CommandInjectableCommandTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class CommandInjectableCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldExecuteInnerCommand()
+        {
+            // Arrange
+            var command = new Mock<ICommand>();
+            var commandInjectableCommand = new CommandInjectableCommand();
+
+            // Act
+            commandInjectableCommand.InjectCommand(command.Object);
+            commandInjectableCommand.Execute();
+
+            // Assert
+            command.Verify(c => c.Execute(), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrow_WhenInnerCommandCouldntInject()
+        {
+            // Arrange
+            var command = new Mock<ICommand>();
+            var commandInjectableCommand = new CommandInjectableCommand();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => commandInjectableCommand.Execute());
+        }
+    }
+}
