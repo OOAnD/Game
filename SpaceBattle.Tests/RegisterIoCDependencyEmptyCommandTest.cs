64b06b35e93907ce0f2e1dd7b6146d8a779efbea
Rau Alexey <rauleha2729@gmail.com>
2024-12-18 03:55:46 +0600

    feat: Добавлен тест для RegisterIoCDependencyEmptyCommand

---
+++ SpaceBattle.Tests/RegisterIoCDependencyEmptyCommandTest.cs
@@ -0,0 +1,27 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyEmptyCommandTests
+    {
+        [Fact]
+        public void Execute_ShouldRegisterEmptyCommandDependency()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var registerCommand = new RegisterIoCDependencyEmptyCommand();
+
+            // Act
+            registerCommand.Execute();
+            var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Empty");
+
+            // Assert
+            Assert.IsType<EmptyCommand>(resolvedCommand);
+        }
+    }
+}
