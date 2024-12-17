6be4506659c3c8035bcd6c23c4eabc4873527e62
Rau Alexey <rauleha2729@gmail.com>
2024-12-18 05:17:51 +0600

    feat: Добавлен тест для RegisterIoCDependencyActionsStop

---
+++ SpaceBattle.Tests/RegisterIoCDependencyActionsStopTest.cs
@@ -0,0 +1,26 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyActionsStopTests
+    {
+        [Fact]
+        public void Execute_ShouldRegisterStopCommandDependency()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var registerCommand = new RegisterIoCDependencyActionsStop();
+
+            // Act
+            registerCommand.Execute();
+            var stopCommand = Ioc.Resolve<ICommand>("Actions.Stop", "testObjectId", "testCommandName");
+
+            // Assert
+            Assert.IsType<StopCommand>(stopCommand);
+        }
+    }
+}
