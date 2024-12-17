18b0614d0b615ae7ae5b0cf1ffefa728560b39e7
ezyxip <anzej2003@gmail.com>
2024-12-17 20:04:40 +0600

    feat: Реализована команда для регистрации зависимости Actions.Start

---
+++ SpaceBattle/RegisterIoCDependencyActionsStart.cs
@@ -0,0 +1,17 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyActionsStart : ICommand
+    {
+        public void Execute()
+        {
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Actions.Start",
+                (object[] args) => new StartCommand((string)args[0], (string)args[1], (object[])args[2])
+            ).Execute();
+        }
+    }
+}
