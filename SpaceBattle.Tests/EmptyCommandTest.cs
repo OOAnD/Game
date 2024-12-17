347500cf183c0ada26706084d9fcf12cf45679e3
Rau Alexey <rauleha2729@gmail.com>
2024-12-18 03:54:33 +0600

    feat: Добавлен тест для EmptyCommand

---
+++ SpaceBattle.Tests/EmptyCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class EmptyCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldDoNothing()
+        {
+            // Arrange
+            var emptyCommand = new EmptyCommand();
+
+            // Act
+            emptyCommand.Execute();
+        }
+    }
+}
