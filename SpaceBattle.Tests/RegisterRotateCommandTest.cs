4087e92a90f2d1db815fe2eba531a994b1127787
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-17 23:22:49 +0600

    feat: Ioc тест

---
+++ SpaceBattle.Tests/RegisterRotateCommandTest.cs
@@ -0,0 +1,25 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterRotateCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldResolve()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var registrator = new RegisterRotateCommand();
+            var rotatingObject = new Mock<IRotating>();
+
+            registrator.Execute();
+
+            var dependency = Ioc.Resolve<ICommand>("Commands.Rotate", rotatingObject.Object);
+            Assert.IsType<RotateCommand>(dependency);
+        }
+    }
+}
