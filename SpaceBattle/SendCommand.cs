dc9306669df218ae6464bfbb0252a2b730b941ab
ezyxip <anzej2003@gmail.com>
2024-12-17 18:48:47 +0600

    feat: Реализована SendCommand

---
+++ SpaceBattle/SendCommand.cs
@@ -0,0 +1,9 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class SendCommand(ICommandReciever reciever, ICommand command) : ICommand
+    {
+        public void Execute() => reciever.RecieveCommand(command);
+    }
+}
