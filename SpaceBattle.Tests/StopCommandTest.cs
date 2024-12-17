6b277f80a55a7355a1d2f8a13027b274ad8feea7
Rau Alexey <rauleha2729@gmail.com>
2024-12-18 03:55:02 +0600

    feat: Добавлен тест для StopCommand

---
+++ SpaceBattle.Tests/StopCommandTest.cs
@@ -0,0 +1,32 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class StopCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldInjectEmptyCommand()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var mockInjectable = new Mock<ICommandInjectable>();
+            var mockEmptyCommand = new Mock<ICommand>();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.Object.GetInjectable", (object[] args) => mockInjectable.Object).Execute();
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Commands.Empty", (object[] args) => mockEmptyCommand.Object).Execute();
+
+            var stopCommand = new StopCommand("testObjectId", "testCommandName");
+
+            // Act
+            stopCommand.Execute();
+
+            // Assert
+            mockInjectable.Verify(i => i.InjectCommand(mockEmptyCommand.Object), Times.Once);
+        }
+    }
+}
