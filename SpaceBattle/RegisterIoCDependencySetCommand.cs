4dafd1e48dc4a37f40ba215f4959220f39f01530
ezyxip <anzej2003@gmail.com>
2024-12-17 20:01:24 +0600

    feat: Добавлена команда для разрешения зависимости Game.Object.Actions.Set

---
+++ SpaceBattle/RegisterIoCDependencySetCommand.cs
@@ -0,0 +1,16 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencySetCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Game.Object.Actions.Set",
+                (object[] args) => new SetCommand((string)args[0], (string)args[1], (ICommand)args[2])
+            ).Execute();
+        }
+    }
+}
