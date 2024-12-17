eb900d6454df82217f7944c046949606d7d3aa7b
Romanova Angelina <7romanovaangelina7@gmail.com>
2024-12-17 20:39:32 +0300

    fix: Исправлено в соответствии с обновлением задания

---
+++ SpaceBattle.Tests/VectorTest.cs
@@ -0,0 +1,143 @@
+namespace SpaceBattle
+{
+    public class VectorTest
+    {
+        [Fact]
+        public void Sum_ShouldBeCorrect()
+        {
+            // Arrange
+            var vector1 = new Vector(3);
+            var vector2 = new Vector(3);
+            var correct = new Vector(3);
+
+            vector1.Set(1, -1, 2);
+            vector2.Set(-1, 1, -2);
+            correct.Set(0, 0, 0);
+
+            // Act
+            var result = vector1 + vector2;
+
+            // Assert
+            Assert.True(result == correct);
+        }
+
+        [Fact]
+        public void Sum_ShouldThrow_WhenVectorsHaveDifferentSize1()
+        {
+            //Arrange
+            var vector1 = new Vector(3);
+            var vector2 = new Vector(2);
+
+            vector1.Set(1, 2, 3);
+            vector2.Set(1, 2);
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => vector1 + vector2);
+        }
+
+        [Fact]
+        public void Sum_ShouldThrow_WhenVectorsHaveDifferentSize2()
+        {
+            //Arrange
+            var vector1 = new Vector(2);
+            var vector2 = new Vector(3);
+
+            vector1.Set(1, 2);
+            vector2.Set(1, 2, 3);
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => vector1 + vector2);
+        }
+
+        [Fact]
+        public void Comparison_ShouldReturnTrue_ForEqualsValues()
+        {
+            // Arrange
+            var vector1 = new Vector(2);
+            var vector2 = new Vector(2);
+
+            vector1.Set(12, 5);
+            vector2.Set(12, 5);
+
+            // Act & Assert
+            Assert.True(vector1.Equals(vector2));
+        }
+
+        [Fact]
+        public void Comparison_ShouldReturnTrue_ForSameValues()
+        {
+            // Arrange
+            var vector1 = new Vector(2);
+            var vector2 = new Vector(2);
+
+            vector1.Set(12, 5);
+            vector2.Set(12, 5);
+
+            // Act & Assert
+            Assert.True(vector1 == vector2);
+        }
+
+        [Fact]
+        public void Comparison_ShouldReturnTrue_ForDifferentEqualsValues()
+        {
+            // Arrange
+            var vector1 = new Vector(2);
+            var vector2 = new Vector(2);
+
+            vector1.Set(12, 5);
+            vector2.Set(12, 6);
+
+            // Act & Assert
+            Assert.False(vector1.Equals(vector2));
+        }
+
+        [Fact]
+        public void Comparison_ShouldReturnTrue_ForDifferentValues()
+        {
+            // Arrange
+            var vector1 = new Vector(2);
+            var vector2 = new Vector(2);
+
+            vector1.Set(12, 5);
+            vector2.Set(12, 6);
+
+            // Act & Assert
+            Assert.True(vector1 != vector2);
+        }
+
+        [Fact]
+        public void Availability_GetHashCode()
+        {
+            // Arrange
+            var vector1 = new Vector(2);
+
+            // Act & Assert
+            Assert.False(vector1.GetHashCode() == 0);
+        }
+
+        [Fact]
+        public void Set_ShouldThrow_WhenIncorrectNumberOfValues()
+        {
+            // Arrange
+            var vector = new Vector(2);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => vector.Set(12));
+        }
+
+        [Fact]
+        public void Set_ShouldWork_WhenCorrectNumberOfValues()
+        {
+            // Arrange
+            var vector = new Vector(2);
+
+            // Act
+            vector.Set(12, 5);
+
+            // Assert
+            var correct = new Vector(2);
+            correct.Set(12, 5);
+            Assert.True(vector == correct);
+        }
+    }
+}
