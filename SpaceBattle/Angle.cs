96bcfb3595bc600f14064c29c2245f11cc57d031
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-18 10:21:46 +0600

    feat: Реализован класс Угол на плоскости

---
+++ SpaceBattle/Angle.cs
@@ -0,0 +1,48 @@
+﻿namespace SpaceBattle
+{
+    public class Angle
+    {
+        public int _numerator;
+        public static int _denominator = 8;
+        public int Numerator
+        {
+            get => _numerator;
+            set => _numerator = ((value % _denominator) + _denominator) % _denominator;
+        }
+        public static int Denominator => _denominator;
+        public Angle(int numerator)
+        {
+            _numerator = ((numerator % _denominator) + _denominator) % _denominator;
+        }
+        public static Angle operator +(Angle thisAngle, Angle otherAngle)
+        {
+            return new Angle(thisAngle.Numerator + otherAngle.Numerator);
+        }
+        public static Angle operator -(Angle thisAngle, Angle otherAngle)
+        {
+            return new Angle(thisAngle.Numerator - otherAngle.Numerator);
+        }
+        public static bool operator ==(Angle thisAngle, Angle otherAngle)
+        {
+            return thisAngle.Numerator == otherAngle.Numerator;
+        }
+        public static bool operator !=(Angle thisAngle, Angle otherAngle)
+        {
+            return thisAngle.Numerator != otherAngle.Numerator;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (Angle)obj;
+            return Numerator == other.Numerator;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_numerator, _denominator);
+        }
+    }
+}
