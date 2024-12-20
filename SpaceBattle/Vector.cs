3f039047968b163c67d75643aca757fdc56cf932
Romanova Angelina <7romanovaangelina7@gmail.com>
2024-12-20 17:11:53 +0300

    feat: Создан Vector

---
+++ SpaceBattle/Vector.cs
@@ -0,0 +1,55 @@
+﻿namespace SpaceBattle
+{
+    public class Vector
+    {
+        private int[] numbers;
+        public int Dimension => numbers.Length;
+
+        public Vector(int dimension)
+        {
+            numbers = new int[dimension];
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            if (v1.Dimension != v2.Dimension)
+            {
+                throw new ArgumentException("Vectors must have the same dimension.");
+            }
+
+            var result = new Vector(v1.Dimension);
+            result.numbers = v1.numbers.Zip(v2.numbers, (a, b) => a + b).ToArray();
+            return result;
+        }
+
+        public void Set(params int[] numbers)
+        {
+            if (numbers.Length != Dimension)
+            {
+                throw new ArgumentException("Number of values must match the dimension of the vector.");
+            }
+
+            this.numbers = numbers;
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return v1.Dimension == v2.Dimension && v1.numbers.SequenceEqual(v2.numbers);
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Vector other && this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return numbers.GetHashCode();
+        }
+    }
+}
