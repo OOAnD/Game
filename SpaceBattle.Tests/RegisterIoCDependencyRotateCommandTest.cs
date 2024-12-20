e05f7d2a1790bc1befa00c43b76a58247d546c6b
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-20 18:17:05 +0600

    feat: Реализована Регистрация зависимости Commands.Rotate в IoC
    Regist

---
+++ SpaceBattle.Tests/RegisterIoCDependencyRotateCommandTest.cs
@@ -0,0 +1,28 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyRotateCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldResolve()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var registrator = new RegisterIoCDependencyRotateCommand();
+            var rotatingObject = new Mock<IRotating>();
+
+            // Act
+            registrator.Execute();
+
+            // Assert
+            var dependency = Ioc.Resolve<ICommand>("Commands.Rotate", rotatingObject.Object);
+            Assert.IsType<RotateCommand>(dependency);
+        }
+    }
+}
---
+++ SpaceBattle/RegisterIoCDependencyRotateCommand.cs
@@ -0,0 +1,16 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyRotateCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.Rotate",
+                (object[] args) => new RotateCommand((IRotating)args[0])
+            ).Execute();
+        }
+    }
+}
