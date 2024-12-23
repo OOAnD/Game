298918099a28843b1e286717858821c7dbafadfa
Timur | IronSmail <timka.smile005@gmail.com>
2024-12-23 15:29:02 +0600

    feat: Создана регистрация зависимости Команды вращения

---
+++ SpaceBattle.Tests/RegisterIoCDependencyRotateCommandTest.cs
@@ -0,0 +1,30 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyRotateCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldResolve()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var rotating = new Mock<IRotating>();
+            var gameObject = new object();
+            var registrator = new RegisterIoCDependencyRotateCommand();
+            Ioc.Resolve<ICommand>("IoC.Register", "Adapters.Rotating", (object[] args) => rotating.Object).Execute();
+
+            // Act
+            registrator.Execute();
+
+            // Assert
+            var dependency = Ioc.Resolve<ICommand>("Commands.Rotate", gameObject);
+            Assert.IsType<RotateCommand>(dependency);
+        }
+    }
+}
