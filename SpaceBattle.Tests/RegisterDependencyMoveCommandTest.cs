6392ddb9ae8e7a6dbc5aab54f8f3a8a2eaf811fd
Romanova Angelina <7romanovaangelina7@gmail.com>
2024-12-23 17:16:36 +0300

    feat: Реализован класс RegisterIoCDependencyMoveCommand

---
+++ SpaceBattle.Tests/RegisterDependencyMoveCommandTest.cs
@@ -0,0 +1,31 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterDependencyMoveCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldRegisterDependencyMoveCommand()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var movingObject = new Mock<IMoving>();
+            var moving = new Mock<IMoving>();
+            var gameObject = new object();
+            var registrator = new RegisterIoCDependencyMoveCommand();
+            Ioc.Resolve<ICommand>("IoC.Register", "Adapters.Moving", (object[] args) => moving.Object).Execute();
+
+            // Act
+            registrator.Execute();
+
+            // Assert
+            var dependency = Ioc.Resolve<ICommand>("Commands.Move", gameObject);
+            Assert.IsType<MoveCommand>(dependency);
+        }
+    }
+}
---
+++ SpaceBattle/RegisterIoCDependencyMoveCommand.cs
@@ -0,0 +1,22 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyMoveCommand : ICommand
+    {
+
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.Move",
+                (object[] args) =>
+                {
+                    var rotating = Ioc.Resolve<IMoving>("Adapters.Moving", args[0]);
+
+                    return new MoveCommand(rotating);
+                }
+            ).Execute();
+        }
+    }
+}
