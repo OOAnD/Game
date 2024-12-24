a7e78d83201458ff2d26ceb9c3b8b1cc9e9b41c6
Rau Alexey <rauleha2729@gmail.com>
2024-12-24 08:32:24 +0600

    fix: Убран лишний тест

--- SpaceBattle.Tests/RegisterIoCDependencyMacroCommandStrategyTest.cs
+++ SpaceBattle.Tests/RegisterIoCDependencyMacroCommandStrategyTest.cs
@@ -55,35 +55,5 @@ namespace SpaceBattle.Tests
             // Assert
             Assert.Equal(mockMacroCommand.Object, resolvedMacroRotate);
         }
-
-        [Fact]
-        public void Execute_ShouldResolveMacroCommandsAsExpected()
-        {
-            // Arrange
-            new InitCommand().Execute();
-            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
-            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
-
-            var mockMacroCommand = new Mock<ICommand>();
-            var mockMoveCommand = new Mock<ICommand>();
-            var mockRotateCommand = new Mock<ICommand>();
-
-            Ioc.Resolve<ICommand>("IoC.Register", "Commands.Macro", (object[] args) => mockMacroCommand.Object).Execute();
-            Ioc.Resolve<ICommand>("IoC.Register", "Specs.Move", (object[] args) => new[] { "MoveCommand" }).Execute();
-            Ioc.Resolve<ICommand>("IoC.Register", "Commands.MoveCommand", (object[] args) => mockMoveCommand.Object).Execute();
-            Ioc.Resolve<ICommand>("IoC.Register", "Specs.Rotate", (object[] args) => new[] { "RotateCommand" }).Execute();
-            Ioc.Resolve<ICommand>("IoC.Register", "Commands.RotateCommand", (object[] args) => mockRotateCommand.Object).Execute();
-
-            var registerCommand = new RegisterIoCDependencyMacroMoveRotate();
-
-            // Act
-            registerCommand.Execute();
-            var resolvedMacroMove = Ioc.Resolve<ICommand>("Macro.Move", new object[] { new object() });
-            var resolvedMacroRotate = Ioc.Resolve<ICommand>("Macro.Rotate", new object[] { new object() });
-
-            // Assert
-            Assert.Equal(mockMacroCommand.Object, resolvedMacroMove);
-            Assert.Equal(mockMacroCommand.Object, resolvedMacroRotate);
-        }
     }
 }
