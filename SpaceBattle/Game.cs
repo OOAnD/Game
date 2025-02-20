92ba026976edeeec84d76683beb59faad588002b
Rau Alexey <rauleha2729@gmail.com>
2025-02-21 01:57:58 +0600

    feat: Создан класс Game

---
+++ SpaceBattle/Game.cs
@@ -0,0 +1,30 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class Game
+    {
+        private readonly IIoCContainer _iocContainer;
+
+        public Game(IIoCContainer iocContainer)
+        {
+            _iocContainer = iocContainer ?? throw new ArgumentNullException(nameof(iocContainer));
+        }
+
+        public void ProcessOrder(IDictionary<string, object> order)
+        {
+            _ = order ?? throw new ArgumentNullException(nameof(order));
+
+            try
+            {
+                var command = _iocContainer.Resolve<ICommand>("Commands.Shoot", order);
+                command.Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при выполнении приказа: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
