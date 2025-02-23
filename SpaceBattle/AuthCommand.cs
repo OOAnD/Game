bceb260bd8f3ca419fe1789034b50b2d9d83e166
Timur | IronSmail <timka.smile005@gmail.com>
2025-02-23 14:47:14 +0600

    feat: Создан класс AuthCommand

---
+++ SpaceBattle/AuthCommand.cs
@@ -0,0 +1,21 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class AuthCommand : ICommand
+    {
+        public string _playerId;
+        public string _objectId;
+        public AuthCommand(string playerId, string objectId)
+        {
+            _playerId = playerId;
+            _objectId = objectId;
+        }
+
+        public void Execute()
+        {
+            var gameItems = Ioc.Resolve<IEnumerable<string>>("Players.GetBelongings", _playerId);
+            var item = gameItems.FirstOrDefault(item => item == _objectId) ?? throw new Exception();
+        }
+    }
+}
