e721357d12a4791ecb321b3168e98374d05f4fce
Romanova Angelina <7romanovaangelina7@gmail.com>
2025-03-10 00:15:24 +0300

    feat: Реализован класс CreateGameObjectStrategy

---
+++ SpaceBattle.Tests/CreateGameObjectStartegyTest.cs
@@ -0,0 +1,49 @@
+﻿namespace SpaceBattle.Test
+{
+    public class CreateGameObjectStrategyTest
+    {
+        [Fact]
+        public void Execute_ShouldAddGameObject_WithUniqueId()
+        {
+            // Arrange
+            var strategy = new CreateGameObjectStrategy();
+            var gameObject = new object();
+
+            // Act
+            var objectId = strategy.Execute(gameObject);
+
+            // Assert
+            var gameObjects = strategy.GetGameObjects();
+            Assert.Single(gameObjects);
+            Assert.Equal(gameObject, gameObjects[objectId]);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrowException_WhenGameObjectIsNull()
+        {
+            // Arrange
+            var strategy = new CreateGameObjectStrategy();
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => strategy.Execute(null!));
+            Assert.Equal("gameObject", exception.ParamName);
+            Assert.Contains("Игровой объект не может быть null.", exception.Message);
+        }
+
+        [Fact]
+        public void Execute_ShouldGenerateUniqueIds_ForMultipleObjects()
+        {
+            // Arrange
+            var strategy = new CreateGameObjectStrategy();
+            var gameObject1 = new object();
+            var gameObject2 = new object();
+
+            // Act
+            var objectId1 = strategy.Execute(gameObject1);
+            var objectId2 = strategy.Execute(gameObject2);
+
+            // Assert
+            Assert.NotEqual(objectId1, objectId2);
+        }
+    }
+}
---
+++ SpaceBattle/CreateGameObjectStartegy.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceBattle
+{
+    public class CreateGameObjectStrategy
+    {
+        private readonly Dictionary<string, object> _gameObjects;
+
+        public CreateGameObjectStrategy()
+        {
+            _gameObjects = [];
+        }
+        public string Execute(object? gameObject)
+        {
+            var objectId = Guid.NewGuid().ToString();
+            _gameObjects.Add(objectId, gameObject ?? throw new ArgumentNullException(nameof(gameObject), "Игровой объект не может быть null."));
+            return objectId;
+        }
+
+        public IDictionary<string, object> GetGameObjects()
+        {
+            return _gameObjects;
+        }
+    }
+}
