67928cb2d8e3c37a1ce5eeb8f684a584579d1d09
Romanova Angelina <7romanovaangelina7@gmail.com>
2025-03-10 00:17:04 +0300

    feat: Реализован класс SaveGameObjectStrategy

---
+++ SpaceBattle.Tests/SaveGameObjectStrategyTest.cs
@@ -0,0 +1,69 @@
+﻿namespace SpaceBattle.Test
+{
+    public class SaveGameObjectStrategyTest
+    {
+        [Fact]
+        public void Execute_ShouldSaveGameObject_WhenInputIsValid()
+        {
+            // Arrange
+            var gameObjects = new Dictionary<string, object>();
+            var saveStrategy = new SaveGameObjectStrategy(gameObjects);
+
+            var objectId = "object1";
+            var gameObject = new object();
+
+            // Act
+            saveStrategy.Execute(objectId, gameObject);
+
+            // Assert
+            Assert.Single(gameObjects);
+            Assert.Equal(gameObject, gameObjects[objectId]);
+        }
+
+        [Fact]
+        public void Execute_ShouldOverwriteGameObject_WhenObjectIdExists()
+        {
+            // Arrange
+            var gameObjects = new Dictionary<string, object>
+            {
+                { "object1", new object() }
+            };
+
+            var saveStrategy = new SaveGameObjectStrategy(gameObjects);
+
+            var objectId = "object1";
+            var newGameObject = new object();
+
+            // Act
+            saveStrategy.Execute(objectId, newGameObject);
+
+            // Assert
+            Assert.Single(gameObjects);
+            Assert.Equal(newGameObject, gameObjects[objectId]);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrowException_WhenObjectIdIsNull()
+        {
+            // Arrange
+            var gameObjects = new Dictionary<string, object>();
+            var saveStrategy = new SaveGameObjectStrategy(gameObjects);
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => saveStrategy.Execute(null!, new object()));
+            Assert.Equal("objectId", exception.ParamName);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrowException_WhenGameObjectIsNull()
+        {
+            // Arrange
+            var gameObjects = new Dictionary<string, object>();
+            var saveStrategy = new SaveGameObjectStrategy(gameObjects);
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => saveStrategy.Execute("object1", null!));
+            Assert.Equal("gameObject", exception.ParamName);
+        }
+    }
+}
---
+++ SpaceBattle/SaveGameObjectStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle
+{
+    public class SaveGameObjectStrategy(IDictionary<string, object> gameObjects)
+    {
+        private readonly IDictionary<string, object> _gameObjects = gameObjects ?? throw new ArgumentNullException(nameof(gameObjects));
+
+        public void Execute(string objectId, object gameObject)
+        {
+            _gameObjects[objectId ?? throw new ArgumentNullException(nameof(objectId), "Идентификатор объекта не может быть null.")] = gameObject ?? throw new ArgumentNullException(nameof(gameObject), "Игровой объект не может быть null.");
+        }
+    }
+}
