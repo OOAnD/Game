8e138a528b7ccafa8c16cd422f3637726fa9d3ff
Romanova Angelina <7romanovaangelina7@gmail.com>
2025-03-10 00:14:11 +0300

    feat: Реализован класс FindGameObjectStrategy

---
+++ SpaceBattle.Tests/FindGameObjectStrategyTest.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+
+namespace SpaceBattle.Test
+{
+    public class FindGameObjectStrategyTest
+    {
+        [Fact]
+        public void Execute_ShouldReturnGameObject_WhenObjectExists()
+        {
+            // Arrange
+            var objectId = "object1";
+            var expectedGameObject = new object();
+
+            var gameObjectsMock = new Mock<IDictionary<string, object>>();
+            gameObjectsMock.Setup(dict => dict.TryGetValue(objectId, out expectedGameObject))
+                          .Returns(true);
+
+            var strategy = new FindGameObjectStrategy(gameObjectsMock.Object);
+
+            // Act
+            var result = strategy.Execute(objectId);
+
+            // Assert
+            Assert.Equal(expectedGameObject, result);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrowException_WhenObjectDoesNotExist()
+        {
+            // Arrange
+            var objectId = "nonExistentObject";
+            object? nullGameObject = null;
+
+            var gameObjectsMock = new Mock<IDictionary<string, object>>();
+            gameObjectsMock.Setup(dict => dict.TryGetValue(objectId, out nullGameObject))
+                          .Returns(false);
+
+            var strategy = new FindGameObjectStrategy(gameObjectsMock.Object);
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => strategy.Execute(objectId));
+            Assert.Equal("Object not found", exception.Message);
+        }
+    }
+}
---
+++ SpaceBattle/FindGameObjectStrategy.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle
+{
+    public class FindGameObjectStrategy(IDictionary<string, object> gameObjects)
+    {
+        private readonly IDictionary<string, object> _gameObjects = gameObjects ?? throw new ArgumentNullException(nameof(gameObjects));
+
+        public object Execute(string objectId)
+        {
+            if (_gameObjects.TryGetValue(objectId, out var gameObject))
+            {
+                return gameObject;
+            }
+
+            throw new Exception("Object not found");
+        }
+    }
+}
