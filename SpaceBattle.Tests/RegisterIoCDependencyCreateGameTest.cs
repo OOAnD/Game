ae4090758858c06b352a05de4a0ea0628c0ab1e5
Rau Alexey <rauleha2729@gmail.com>
2025-03-11 07:59:27 +0600

    feat: Добавлен RegisterIoCDependencyCreateGame

---
+++ SpaceBattle.Tests/RegisterIoCDependencyCreateGameTest.cs
@@ -0,0 +1,38 @@
+﻿using App.Scopes;
+using App;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyCreateGameTest
+    {
+        [Fact]
+        public void Execute_RegistersNextCommandDependency()
+        {
+            // Arrange
+            new InitCommand().Execute(); 
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var mockQueue = new Mock<IQueue>();
+            var mockCommand = new Mock<ICommand>();
+
+            mockQueue.Setup(q => q.Take()).Returns(mockCommand.Object);
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Game.Queue",
+                (object[] _) => mockQueue.Object).Execute();
+
+            var registerNextCommand = new RegisterIoCDependencyNextCommand();
+
+            // Act
+            registerNextCommand.Execute();
+
+            // Assert
+            var nextCommand = Ioc.Resolve<ICommand>("Commands.GetNextCommand");
+            Assert.NotNull(nextCommand);
+            Assert.Equal(mockCommand.Object, nextCommand);
+        }
+    }
+}
---
+++ SpaceBattle/RegisterIoCDependencyCreateGame.cs
@@ -0,0 +1,15 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyCreateGame : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Game.Create",
+                (object[] args) => new Game(args[0])).Execute();
+        }
+    }
+}
