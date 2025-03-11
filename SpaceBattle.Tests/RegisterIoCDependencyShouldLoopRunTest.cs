ef5b007377de5b64d3027ec10b118932bc741b34
Rau Alexey <rauleha2729@gmail.com>
2025-03-11 08:00:24 +0600

    feat: Добавлен RegisterIoCDependencyShouldLoopRun

---
+++ SpaceBattle.Tests/RegisterIoCDependencyShouldLoopRunTest.cs
@@ -0,0 +1,33 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyShouldLoopRunTests
+    {
+        [Fact]
+        public void Execute_RegistersShouldLoopRunDependency()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var mockQueueCount = new Mock<IQueueCount>();
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Game.Queue",
+                (object[] _) => mockQueueCount.Object).Execute();
+
+            var registerShouldLoopRun = new RegisterIoCDependencyShouldLoopRun();
+
+            // Act
+            registerShouldLoopRun.Execute();
+
+            // Assert
+            var shouldLoopRun = Ioc.Resolve<bool>("Game.ShouldLoopRun");
+            Assert.False(shouldLoopRun);
+        }
+    }
+}
---
+++ SpaceBattle/RegisterIoCDependencyShouldLoopRun.cs
@@ -0,0 +1,18 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyShouldLoopRun : ICommand
+    {
+        public void Execute()
+        {
+            var queueCount = Ioc.Resolve<IQueueCount>("Game.Queue");
+            var gameLoop = new GameLoop(queueCount);
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Game.ShouldLoopRun",
+                (object[] _) => (object)gameLoop.ShouldLoopRun()).Execute();
+        }
+    }
+}
