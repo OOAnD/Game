e8a2969b7218fc5a393e097da0a7c76f2c5550eb
Rau Alexey <rauleha2729@gmail.com>
2025-03-11 07:59:53 +0600

    feat: Добавлен RegisterIoCDependencyNextCommand

---
+++ SpaceBattle/RegisterIoCDependencyNextCommand.cs
@@ -0,0 +1,16 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyNextCommand : ICommand
+    {
+        public void Execute()
+        {
+            var q = Ioc.Resolve<IQueue>("Game.Queue");
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.GetNextCommand",
+                (object[] _) => q.Take()).Execute();
+        }
+    }
+}
