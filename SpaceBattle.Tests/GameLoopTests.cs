c37f92408bee36f76d9a3b254dbd95da545570ab
Rau Alexey <rauleha2729@gmail.com>
2025-03-11 07:58:28 +0600

    feat: Добавлен GameLoop

---
+++ SpaceBattle.Tests/GameLoopTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+
+namespace SpaceBattle.Tests
+{
+    public class GameLoopTests
+    {
+        [Fact]
+        public void ShouldLoopRun_ReturnsTrue_WhenQueueIsNotEmpty()
+        {
+            // Arrange
+            var mockQueueCount = new Mock<IQueueCount>();
+            mockQueueCount.Setup(q => q.Count()).Returns(1);
+
+            var gameLoop = new GameLoop(mockQueueCount.Object);
+
+            // Act
+            var result = gameLoop.ShouldLoopRun();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldLoopRun_ReturnsFalse_WhenQueueIsEmpty()
+        {
+            // Arrange
+            var mockQueueCount = new Mock<IQueueCount>();
+            mockQueueCount.Setup(q => q.Count()).Returns(0);
+
+            var gameLoop = new GameLoop(mockQueueCount.Object);
+
+            // Act
+            var result = gameLoop.ShouldLoopRun();
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ SpaceBattle/GameLoop.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle
+{
+    public class GameLoop
+    {
+        private readonly IQueueCount _queueCount;
+
+        public GameLoop(IQueueCount queueCount)
+        {
+            _queueCount = queueCount;
+        }
+
+        public bool ShouldLoopRun()
+        {
+            return _queueCount.Count() > 0;
+        }
+    }
+}
