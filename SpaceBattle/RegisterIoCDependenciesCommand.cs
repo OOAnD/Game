637928c0c151f6467405bd22567e42c24631d992
Romanova Angelina <7romanovaangelina7@gmail.com>
2025-03-25 00:56:00 +0300

    feat: Реализован класс RegisterGameDependenciesCommand

---
+++ SpaceBattle/RegisterIoCDependenciesCommand.cs
@@ -0,0 +1,40 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterGameDependenciesCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Strategies.Create.Torpedo",
+                (object[] _) => CreateTorpedoStrategy.Create()
+            ).Execute();
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.GameObject.Save",
+                (object[] args) => new SaveGameObjectCommand((IDictionary<string, object>)args[0])
+            ).Execute();
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.GameObject.Remove",
+                (object[] args) => new RemoveGameObject((IDictionary<string, object>)args[0])
+            ).Execute();
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Strategies.Find.GameObject",
+                (object[] args) => new FindGameObjectStrategy((string)args[0])
+            ).Execute();
+
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Strategies.Find.PlayerShips",
+                (object[] args) => new PlayersShipFindStrategy((string)args[0])
+            ).Execute();
+        }
+    }
+}
