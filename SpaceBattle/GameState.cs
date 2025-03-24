206bef39e4f60522d8acf18283f53f90a45e2a01
Rau Alexey <rauleha2729@gmail.com>
2025-03-24 17:09:21 +0600

    feat: Добавлен GameState

---
+++ SpaceBattle/GameState.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceBattle
+{
+    public class GameState
+    {
+        private bool _isRunning;
+
+        public GameState()
+        {
+            _isRunning = true;
+        }
+
+        public bool IsRunning()
+        {
+            return _isRunning;
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+    }
+}
