2f0e14c6aa3c86a4f67e64cbbee2a1321a912bde
Romanova Angelina <7romanovaangelina7@gmail.com>
2025-03-25 11:01:20 +0300

    fix: Исправлено покрытие

--- SpaceBattle.Tests/RegisterIocDependencyRemoveGameObjectCommand.cs
+++ SpaceBattle.Tests/RegisterIocDependencyRemoveGameObjectCommand.cs
@@ -6,30 +6,22 @@ namespace SpaceBattle.Tests
     public class RegisterIocDependencyRemoveGameObjectCommandTest
     {
         [Fact]
-        public void Execute_ShouldRemoveGameObject()
+        public void Execute_ShouldRegisterRemoveGameObjectDependency()
         {
             // Arrange
             new InitCommand().Execute();
             var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
             Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
 
-            var testId = "test_obj_123";
-            var gameObject = new Dictionary<string, object> { ["id"] = testId };
-            var objectsDict = new Dictionary<string, IDictionary<string, object>>
-            {
-                [testId] = gameObject
-            };
-
-            Ioc.Resolve<ICommand>("IoC.Register", "Game.Objects.GetAll",
-                (Func<object[], IDictionary<string, IDictionary<string, object>>>)(_ => objectsDict)).Execute();
-
-            var command = new RemoveGameObjectCommand(gameObject);
+            var command = new RegisterIocDependencyRemoveGameObjectCommand();
 
             // Act
             command.Execute();
 
             // Assert
-            Assert.False(objectsDict.ContainsKey(testId));
+            var gameObject = new Dictionary<string, object> { ["id"] = "test" };
+            var removeCommand = Ioc.Resolve<ICommand>("Game.Objects.Remove", gameObject);
+            Assert.IsType<RemoveGameObjectCommand>(removeCommand);
         }
     }
 }
