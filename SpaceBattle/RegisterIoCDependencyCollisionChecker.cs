c27db203e90f6587a8bce7cc0ac77b2b0156b6e9
ezyxip <anzej2003@gmail.com>
2025-05-05 12:52:04 +0600

    feat: Добавлена команда для регистрации зависимости PolygonCollisionCheckers

---
+++ SpaceBattle/RegisterIoCDependencyCollisionChecker.cs
@@ -0,0 +1,21 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyCollisionChecker : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>("IoC.Register", "PolygonCollisionCheckers", (object[] args) =>
+            {
+                var shape1 = Ioc.Resolve<IEnumerable<Point>>("Shapes.Get", (string)args[0]);
+                var shape2 = Ioc.Resolve<IEnumerable<Point>>("Shapes.Get", (string)args[1]);
+
+                var checkerByRays = Ioc.Resolve<ICollisionChecker>("PolygonCollisionChecker.ByRays", shape1, shape2);
+                var checkerBySAT = Ioc.Resolve<ICollisionChecker>("PolygonCollisionChecker.BySAT", shape1, shape2);
+
+                return (IEnumerable<ICollisionChecker>)[checkerBySAT, checkerByRays];
+            }).Execute();
+        }
+    }
+}
