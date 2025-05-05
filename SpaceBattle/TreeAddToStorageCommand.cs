252567d594b2bb830583471cfa88e4b4562061a0
Timur | IronSmail <timka.smile005@gmail.com>
2025-05-05 16:14:39 +0600

    feat: Создана команда добавления дерева в хранилище

---
+++ SpaceBattle/TreeAddToStorageCommand.cs
@@ -0,0 +1,26 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class TreeAddToStorageCommand : ICommand
+    {
+        public string _form1;
+        public string _form2;
+
+        public QuadrupleTree _tree;
+        public Dictionary<(string, string), QuadrupleTree> _storage;
+
+        public TreeAddToStorageCommand(object[] args)
+        {
+            _form1 = (string)args[0];
+            _form2 = (string)args[1];
+            _tree = (QuadrupleTree)args[2];
+            _storage = (Dictionary<(string, string), QuadrupleTree>)args[3];
+        }
+
+        public void Execute()
+        {
+            _storage[(_form1, _form2)] = _tree;
+        }
+    }
+}
