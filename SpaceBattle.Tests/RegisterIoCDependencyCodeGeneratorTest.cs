4644699a2731ae58e859b6bb5aedfdc58173099f
ezyxip <anzej2003@gmail.com>
2025-05-05 16:39:20 +0600

    feat: Создана зависимость для генерации кода адаптера

---
+++ SpaceBattle.Tests/RegisterIoCDependencyCodeGeneratorTest.cs
@@ -0,0 +1,73 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyCodeGeneratorTest
+    {
+        [Fact]
+        public void Execute_ShouldCorrectRegistry()
+        {
+            //Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            //Act
+            var registrator = new RegisterIoCDependencyGenerateAdapterCode();
+            registrator.Execute();
+
+            //Assert
+            var code = Ioc.Resolve<string>("Adapters.CodeGenerator", typeof(ITest1));
+            Assert.IsType<string>(code);
+        }
+
+        [Fact]
+        public void Execute_ShouldReturnCorrectCode()
+        {
+            //Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+            var correctCode = @"class ITest1Adapter : ITest1
+{
+    private readonly IDictionary<string, object> _dict;
+
+    public ITest1Adapter(IDictionary<string, object> dict)
+    {
+        _dict = dict ?? throw new ArgumentNullException(nameof(dict));
+    }
+
+
+    public String Prop1
+    {
+        get => (String)_dict[" + "\"Prop1\"" + @"];
+    
+            set => _dict[" + "\"Prop1\"" + @"] = value;
+        
+    }
+
+    public Int32 Prop2
+    {
+        get => (Int32)_dict[" + "\"Prop2\"" + @"];
+    
+    }
+
+}";
+
+            //Act
+            var registrator = new RegisterIoCDependencyGenerateAdapterCode();
+            registrator.Execute();
+
+            //Assert
+            var code = Ioc.Resolve<string>("Adapters.CodeGenerator", typeof(ITest1));
+            Assert.Equal(correctCode, code);
+        }
+    }
+
+    internal interface ITest1
+    {
+        string Prop1 { get; set; }
+        int Prop2 { get; }
+    }
+}
---
+++ SpaceBattle/RegisterIoCDependencyCodeGenerator.cs
@@ -0,0 +1,48 @@
+﻿using App;
+using Scriban;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyGenerateAdapterCode : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>("IoC.Register", "Adapters.CodeGenerator", (object[] args) =>
+            {
+                var interfaceType = (Type)args[0];
+                var pattern = @"class {{ Interface.Name }}Adapter : {{ Interface.Name }}
+{
+    private readonly IDictionary<string, object> _dict;
+
+    public {{ Interface.Name }}Adapter(IDictionary<string, object> dict)
+    {
+        _dict = dict ?? throw new ArgumentNullException(nameof(dict));
+    }
+
+{{ for prop in InterfaceProperties }}
+    public {{ prop.PropertyType.Name }} {{ prop.Name }}
+    {
+        get => ({{ prop.PropertyType.Name }})_dict[" + "\"{{ prop.Name }}\"" + @"];
+    {{ if prop.CanWrite }}
+        set => _dict[" + "\"{{ prop.Name }}\"" + @"] = value;
+    {{ end }}
+    }
+{{ end }}
+}";
+
+                var template = Template.Parse(pattern);
+                var model = new
+                {
+                    Interface = interfaceType,
+                    InterfaceProperties = interfaceType.GetProperties()
+                };
+
+                // Рендерим шаблон
+                var generatedCode = template.Render(model, member => member.Name);
+
+                return generatedCode;
+
+            }).Execute();
+        }
+    }
+}
