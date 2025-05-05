277519a3de5a9c3a85d55e57ff0865ff251be43f
ezyxip <anzej2003@gmail.com>
2025-05-05 12:40:34 +0600

    feat: Добавлена команда для регистрации зависимости PolygonCollisionChecker.ByRays

---
+++ SpaceBattle/RegisterIoCDependencyRayCollisionChecker.cs
@@ -0,0 +1,15 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyRayCollisionChecker : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>("IoC.Register", "PolygonCollisionChecker.ByRays", (object[] args) =>
+            {
+                return new PolygonCollisionCheckerByRays((IEnumerable<Point>)args[0], (IEnumerable<Point>)args[1]);
+            }).Execute();
+        }
+    }
+}
