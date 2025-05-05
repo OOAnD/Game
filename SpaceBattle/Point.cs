1e97ff64888a20ce4fed33e994f4c63133b6a235
ezyxip <anzej2003@gmail.com>
2025-05-05 11:32:22 +0600

    feat: Создан вспомогательный класс Point

---
+++ SpaceBattle/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceBattle
+{
+    public record Point(int X, int Y)
+    {
+        public static Point operator +(Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y);
+        public static Point operator -(Point p) => new Point(-p.X, -p.Y);
+        public static Point operator -(Point p1, Point p2) => p1 + (-p2);
+    }
+}
