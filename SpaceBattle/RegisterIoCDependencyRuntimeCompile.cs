b2b11d03116264d6708842fd9935565db9bbff80
ezyxip <anzej2003@gmail.com>
2025-05-19 23:58:59 +0600

    feat: Добавлена зависимость RuntimeCompiler

---
+++ SpaceBattle/RegisterIoCDependencyRuntimeCompile.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using App;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace SpaceBattle;
+
+public class RegisterIoCDependencyRuntimeCompile : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<ICommand>(
+            "IoC.Register",
+            "RuntimeCompiler",
+            (object[] args) =>
+            {
+                var code = (string)args[0];
+                var fullClassName = (string)args[1];
+
+                var references = AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
+                    .Select(a => MetadataReference.CreateFromFile(a.Location))
+                    .ToList();
+
+                references.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+                references.Add(MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location));
+
+                var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+                var compilation = CSharpCompilation.Create(
+                    assemblyName: Guid.NewGuid().ToString(),
+                    syntaxTrees: new[] { syntaxTree },
+                    references: references,
+                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                );
+
+                using var ms = new MemoryStream();
+                var result = compilation.Emit(ms);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                var assembly = Assembly.Load(ms.ToArray());
+                var type = assembly.GetType(fullClassName)!;
+
+                return type;
+            }
+        ).Execute();
+    }
+}
