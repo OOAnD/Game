46f753fce7418446f7bc4dc288875b4a59d727b5
PerfectVlad <vlad-log03@mail.ru>
2025-05-20 01:55:29 +0600

    feat: Добавлены запись и чтение

--- SpaceBattle/QuadrupleFileProcessing.cs
+++ SpaceBattle/QuadrupleFileProcessing.cs
@@ -1,76 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace SpaceBattle;
 
+
 public class QuadrupleFileProcessing
 {
-    private static int countOfOutput = 0;
 
-
-    public static Stream Write(List<(int, int, int, int)> collection, string outputPath)
+    public static void Write(List<(int, int, int, int)> collection, string filePath)
     {
-        if (collection == null)
-            throw new ArgumentNullException(nameof(collection), "Collection cannot be null");
-
-        if (string.IsNullOrEmpty(outputPath))
-            throw new ArgumentException("Output path cannot be null or empty", nameof(outputPath));
-
-        var destinationFilePath = Path.Combine(outputPath, $"output{++countOfOutput}.bin");
-
-        using (var fileStream = File.Create(destinationFilePath))
-        using (var writer = new BinaryWriter(fileStream))
+        using var fileStream = File.Create(filePath);
+        using var writer = new BinaryWriter(fileStream);
+        
+        writer.Write(collection.Count);
+        
+        foreach (var (a, b, c, d) in collection)
         {
-            writer.Write(collection.Count);
-
-            foreach (var (item1, item2, item3, item4) in collection)
-            {
-                writer.Write(item1);
-                writer.Write(item2);
-                writer.Write(item3);
-                writer.Write(item4);
-            }
+            writer.Write(a);
+            writer.Write(b);
+            writer.Write(c);
+            writer.Write(d);
         }
-
-        return new FileStream(destinationFilePath, FileMode.Open);
     }
 
-    public static List<(int, int, int, int)> Read(Stream stream)
+
+    public static List<(int, int, int, int)> Read(string filePath)
     {
-        if (stream == null)
-            throw new ArgumentNullException(nameof(stream), "Input stream cannot be null");
-
         var result = new List<(int, int, int, int)>();
-
-        using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
+        
+        using var fileStream = File.OpenRead(filePath);
+        using var reader = new BinaryReader(fileStream);
+        
+        var count = reader.ReadInt32();
+        result.Capacity = count;
+        
+        for (var i = 0; i < count; i++)
         {
-            try
-            {
-                var count = reader.ReadInt32();
-                result.Capacity = count;
-
-                for (var i = 0; i < count; i++)
-                {
-                    var item1 = reader.ReadInt32();
-                    var item2 = reader.ReadInt32();
-                    var item3 = reader.ReadInt32();
-                    var item4 = reader.ReadInt32();
-
-                    result.Add((item1, item2, item3, item4));
-                }
-            }
-            catch (EndOfStreamException ex)
-            {
-                throw new InvalidDataException("Unexpected end of file", ex);
-            }
-            catch (IOException ex)
-            {
-                throw new InvalidDataException("Error reading file", ex);
-            }
+            result.Add((
+                reader.ReadInt32(),
+                reader.ReadInt32(),
+                reader.ReadInt32(),
+                reader.ReadInt32()
+            ));
         }
-
+        
         return result;
     }
 }
