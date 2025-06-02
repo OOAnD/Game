e68f0f9c9e02d1af20390dec2ff2f6ff69b05b59
Rau Alexey <rauleha2729@gmail.com>
2025-06-03 04:47:33 +0600

    feat: Зарегистрирована стратегия адаптера в IoC контейнере

---
+++ SpaceBattle/RegisterIoCDependencyAdapterStrategyCommand.cs
@@ -0,0 +1,19 @@
+﻿using App;
+
+namespace SpaceBattle
+{
+    public class RegisterIoCDependencyAdapterStrategyCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<ICommand>(
+                "IoC.Register",
+                "Adapter.Strategy",
+                (object[] args) => new CustomAdapter(
+                    (IDictionary<string, object>)args[0],
+                    (IDictionary<string, Func<object>>)args[1]
+                )
+            ).Execute();
+        }
+    }
+}
