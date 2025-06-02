fac484afb388ebf6d5ecdfbb2cf5da62931af4d6
Rau Alexey <rauleha2729@gmail.com>
2025-06-03 02:51:45 +0600

    feat: Зарегистрирована стратегия адаптера в IoC контейнере

---
+++ SpaceBattle.Tests/RegisterIoCDependencyCustomBehaviorObjectWrapperTest.cs
@@ -0,0 +1,34 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Tests
+{
+    public class RegisterIoCDependencyAdapterStrategyTests
+    {
+        [Fact]
+        public void Execute_ShouldRegisterAdapterStrategy()
+        {
+            // Arrange
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+
+            var sourceData = new Dictionary<string, object>();
+            var customBehaviors = new Dictionary<string, Func<object>>();
+
+            var registrator = new RegisterIoCDependencyAdapterStrategyCommand();
+
+            // Act
+            registrator.Execute();
+
+            // Assert
+            var adapter = Ioc.Resolve<IDictionary<string, object>>(
+                "Adapter.Strategy",
+                sourceData,
+                customBehaviors
+            );
+
+            Assert.IsType<CustomAdapter>(adapter);
+        }
+    }
+}
